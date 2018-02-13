9fdd82e72d335fb0ac3f85de26677d482dd82281
noc <noc@noc-B150-HD3P>
2018-02-13 18:32:47 +0000

    Primeros pasos con la cola

---
+++ mytail.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mytail.cpp
+ * Author: noc
+ * 
+ * Created on 13 de febrero de 2018, 18:28
+ */
+
+#include "mytail.h"
+
+mytail::mytail() {
+}
+
+mytail::mytail(const mytail& orig) {
+}
+
+mytail::~mytail() {
+}
+
---
+++ mytail.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   mytail.h
+ * Author: noc
+ *
+ * Created on 13 de febrero de 2018, 18:28
+ */
+
+#ifndef MYTAIL_H
+#define MYTAIL_H
+
+class mytail {
+public:
+    mytail();
+    mytail(const mytail& orig);
+    virtual ~mytail();
+private:
+
+};
+
+#endif /* MYTAIL_H */
+
